7a4434cd4f8ae553f944f3cf27e0b021ba442415
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-11-29 10:58:38 -0300

    add role type not found exception

--- ms-user/src/main/java/com/project/challenge/msuser/infra/exceptions/handlers/UserExceptionHandler.java
+++ ms-user/src/main/java/com/project/challenge/msuser/infra/exceptions/handlers/UserExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.project.challenge.msuser.infra.exceptions.RoleTypeNotFound;
 import com.project.challenge.msuser.infra.exceptions.UserTypeNotFound;
 import com.project.challenge.msuser.infra.exceptions.DTOs.BasicResponseExceptionDTO;
 
@@ -17,6 +18,14 @@ public class UserExceptionHandler {
 
     @ExceptionHandler(UserTypeNotFound.class)
     public ResponseEntity<BasicResponseExceptionDTO> userTypeNotFound(UserTypeNotFound e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        BasicResponseExceptionDTO response = new BasicResponseExceptionDTO(status.name(), e.getMessage(),
+                Instant.now().toString());
+        return ResponseEntity.status(status).body(response);
+    }
+
+    @ExceptionHandler(RoleTypeNotFound.class)
+    public ResponseEntity<BasicResponseExceptionDTO> roleTypeNotFound(RoleTypeNotFound e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
         BasicResponseExceptionDTO response = new BasicResponseExceptionDTO(status.name(), e.getMessage(),
                 Instant.now().toString());
