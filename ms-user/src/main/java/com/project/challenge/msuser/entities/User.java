f85877f5014e86efde135be90a22991448b279fb
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-11-29 10:43:52 -0300

    Create User.java

---
+++ ms-user/src/main/java/com/project/challenge/msuser/entities/User.java
@@ -0,0 +1,50 @@
+package com.project.challenge.msuser.entities;
+
+import com.project.challenge.msuser.entities.enumerations.UserType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+
+    @Column(unique = true, nullable = false)
+    private String cpfCNPJ;
+    @Column(nullable = false)
+    private UserType userType;
+    private Double balance;
+
+    public User() {
+    }
+
+    public User(String cpfCNPJ, UserType userType, Double balance) {
+        this.cpfCNPJ = cpfCNPJ;
+        this.userType = userType;
+        this.balance = balance;
+    }
+
+    public String getCpfCNPJ() {
+        return cpfCNPJ;
+    }
+
+    public void setCpfCNPJ(String cpfCNPJ) {
+        this.cpfCNPJ = cpfCNPJ;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+}
