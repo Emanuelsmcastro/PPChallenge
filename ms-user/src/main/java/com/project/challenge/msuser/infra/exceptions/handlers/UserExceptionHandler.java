ed4b847c630536435d72a891df64a72448a03dc5
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-11-29 10:36:08 -0300

    Create UserExceptionHandler.java

---
+++ ms-user/src/main/java/com/project/challenge/msuser/infra/exceptions/handlers/UserExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.project.challenge.msuser.infra.exceptions.handlers;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.project.challenge.msuser.infra.exceptions.UserTypeNotFound;
+import com.project.challenge.msuser.infra.exceptions.DTOs.BasicResponseExceptionDTO;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class UserExceptionHandler {
+
+    @ExceptionHandler(UserTypeNotFound.class)
+    public ResponseEntity<BasicResponseExceptionDTO> userTypeNotFound(UserTypeNotFound e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        BasicResponseExceptionDTO response = new BasicResponseExceptionDTO(status.name(), e.getMessage(),
+                Instant.now().toString());
+        return ResponseEntity.status(status).body(response);
+    }
+}
