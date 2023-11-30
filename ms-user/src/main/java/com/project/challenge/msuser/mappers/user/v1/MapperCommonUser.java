d4f76807fb856a2b372fdad3472ac796043def88
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-11-30 11:50:12 -0300

    Create MapperCommonUser.java

---
+++ ms-user/src/main/java/com/project/challenge/msuser/mappers/user/v1/MapperCommonUser.java
@@ -0,0 +1,38 @@
+package com.project.challenge.msuser.mappers.user.v1;
+
+import java.util.List;
+
+import org.springframework.util.ObjectUtils;
+
+import com.project.challenge.msuser.DTOs.user.v1.CommonUserDTO;
+import com.project.challenge.msuser.entities.CommonUser;
+import com.project.challenge.msuser.interfaces.Mapper;
+
+public class MapperCommonUser implements Mapper<CommonUserDTO, CommonUser> {
+    private static final MapperCommonUser mapper = new MapperCommonUser();
+
+    public MapperCommonUser() {
+
+    }
+
+    public static MapperCommonUser getInstance() {
+        return mapper;
+    }
+
+    @Override
+    public CommonUserDTO toDTO(CommonUser entity) {
+        if (ObjectUtils.isEmpty(entity))
+            return null;
+        CommonUserDTO dto = new CommonUserDTO();
+        dto.setUuid(entity.getUuid());
+        dto.setTransferLimit(entity.getTransferLimit());
+        return dto;
+    }
+
+    @Override
+    public List<CommonUserDTO> toDTOList(List<CommonUser> entities) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toDTOList'");
+    }
+
+}
