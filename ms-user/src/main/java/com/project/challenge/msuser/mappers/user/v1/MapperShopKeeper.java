ba46a463b7058834dfd6ae2422f156223d9cede9
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-11-30 11:50:10 -0300

    Create MapperShopKeeper.java

---
+++ ms-user/src/main/java/com/project/challenge/msuser/mappers/user/v1/MapperShopKeeper.java
@@ -0,0 +1,38 @@
+package com.project.challenge.msuser.mappers.user.v1;
+
+import java.util.List;
+
+import org.springframework.util.ObjectUtils;
+
+import com.project.challenge.msuser.DTOs.user.v1.ShopKeeperDTO;
+import com.project.challenge.msuser.entities.ShopKeeper;
+import com.project.challenge.msuser.interfaces.Mapper;
+
+public class MapperShopKeeper implements Mapper<ShopKeeperDTO, ShopKeeper> {
+    private static final MapperShopKeeper mapper = new MapperShopKeeper();
+
+    public MapperShopKeeper() {
+
+    }
+
+    public static MapperShopKeeper getInstance() {
+        return mapper;
+    }
+
+    @Override
+    public ShopKeeperDTO toDTO(ShopKeeper entity) {
+        if (ObjectUtils.isEmpty(entity))
+            return null;
+        ShopKeeperDTO dto = new ShopKeeperDTO();
+        dto.setUuid(entity.getUuid());
+        dto.setYearsInTheCompany(entity.getYearsInTheCompany());
+        return dto;
+    }
+
+    @Override
+    public List<ShopKeeperDTO> toDTOList(List<ShopKeeper> entities) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toDTOList'");
+    }
+
+}
