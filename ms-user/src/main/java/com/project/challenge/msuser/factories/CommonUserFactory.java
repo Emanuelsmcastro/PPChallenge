94da46ad981d11dd673b6152264ef7aaaa51eab7
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-11-30 10:05:23 -0300

    Create CommonUserFactory.java

---
+++ ms-user/src/main/java/com/project/challenge/msuser/factories/CommonUserFactory.java
@@ -0,0 +1,42 @@
+package com.project.challenge.msuser.factories;
+
+import java.util.UUID;
+
+import com.project.challenge.msuser.entities.CommonUser;
+
+public class CommonUserFactory {
+    private static CommonUserFactory instance = new CommonUserFactory();
+    private CommonUser user = new CommonUser();
+
+    public CommonUserFactory() {
+
+    }
+
+    public static CommonUserFactory getInstance() {
+        return instance;
+    }
+
+    public CommonUserFactory setId(Long value) {
+        user.setId(value);
+        return this;
+    }
+
+    public CommonUserFactory setUuid(UUID uuid) {
+        user.setUuid(uuid.toString());
+        return this;
+    }
+
+    public CommonUserFactory autoUuid() {
+        user.setUuid(UUID.randomUUID().toString());
+        return this;
+    }
+
+    public CommonUserFactory setTransferLimit(Double limit) {
+        user.setTransferLimit(limit);
+        return this;
+    }
+
+    public CommonUser build() {
+        return user;
+    }
+}
