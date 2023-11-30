b9c5dd0009806cfc322a3be30a89cafe812cd6da
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-11-30 10:05:26 -0300

    Create ShopKeeperFactory.java

---
+++ ms-user/src/main/java/com/project/challenge/msuser/factories/ShopKeeperFactory.java
@@ -0,0 +1,42 @@
+package com.project.challenge.msuser.factories;
+
+import java.util.UUID;
+
+import com.project.challenge.msuser.entities.ShopKeeper;
+
+public class ShopKeeperFactory {
+    private static ShopKeeperFactory instance = new ShopKeeperFactory();
+    private ShopKeeper user = new ShopKeeper();
+
+    public ShopKeeperFactory() {
+
+    }
+
+    public static ShopKeeperFactory getInstance() {
+        return instance;
+    }
+
+    public ShopKeeperFactory setId(Long value) {
+        user.setId(value);
+        return this;
+    }
+
+    public ShopKeeperFactory setUuid(UUID uuid) {
+        user.setUuid(uuid.toString());
+        return this;
+    }
+
+    public ShopKeeperFactory autoUuid() {
+        user.setUuid(UUID.randomUUID().toString());
+        return this;
+    }
+
+    public ShopKeeperFactory setYearsInTheCompany(Integer years) {
+        user.setYearsInTheCompany(years);
+        return this;
+    }
+
+    public ShopKeeper build() {
+        return user;
+    }
+}
