9614dd5bee6cd5861c0889a730d29a996371a1a2
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-01 10:33:05 -0300

    Create MsTransferApplication.java

---
+++ ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/MsTransferApplication.java
@@ -0,0 +1,13 @@
+package com.project.challenge.mstransfer.mstransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTransferApplication.class, args);
+	}
+
+}
