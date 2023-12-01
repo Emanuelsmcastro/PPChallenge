d4d45ca5bd890fc710e4435bcac52bddf4709d2c
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-01 10:33:02 -0300

    Create TransferStatus.java

---
+++ ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/enumerations/TransferStatus.java
@@ -0,0 +1,27 @@
+package com.project.challenge.mstransfer.mstransfer.enumerations;
+
+import com.project.challenge.mstransfer.mstransfer.infra.exceptions.TransferStatusNotValid;
+
+public enum TransferStatus {
+    PENDING(0),
+    SUCCESS(1),
+    FAIL(2);
+
+    private Integer value;
+
+    TransferStatus(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public TransferStatus valueOf(Integer value) {
+        for (TransferStatus status : values()) {
+            if (status.getValue().equals(value))
+                return status;
+        }
+        throw new TransferStatusNotValid(String.format("Status (%d) not valid.", value));
+    }
+}
