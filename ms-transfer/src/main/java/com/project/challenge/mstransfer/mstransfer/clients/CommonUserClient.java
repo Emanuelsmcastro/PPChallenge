522f6b639fa1dd0e52e9a31d2cf62e1909ec68ea
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-03 05:04:47 -0300

    Create CommonUserClient.java

---
+++ ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/clients/CommonUserClient.java
@@ -0,0 +1,17 @@
+package com.project.challenge.mstransfer.mstransfer.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.project.challenge.mstransfer.mstransfer.DTOs.user.v1.CommonUserDTO;
+
+@Component
+@FeignClient(name = "ms-user", path = "ppchallenge/v1/common-users")
+public interface CommonUserClient {
+
+    @GetMapping(value = "/{uuid}")
+    ResponseEntity<CommonUserDTO> findByUuid(@PathVariable(name = "uuid") String uuid);
+}
