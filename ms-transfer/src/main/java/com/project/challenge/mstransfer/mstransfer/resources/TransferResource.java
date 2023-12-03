76ec9f95319f966321c04ea035ec801d185ed3c0
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-03 08:47:12 -0300

    Add post method to create transfer

--- ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/resources/TransferResource.java
+++ ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/resources/TransferResource.java
@@ -1,13 +1,17 @@
 package com.project.challenge.mstransfer.mstransfer.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project.challenge.mstransfer.mstransfer.DTOs.user.transfer.v1.TransferDTO;
+import com.project.challenge.mstransfer.mstransfer.DTOs.transfer.v1.TransferDTO;
+import com.project.challenge.mstransfer.mstransfer.DTOs.transfer.v1.TransferRequestDTO;
 import com.project.challenge.mstransfer.mstransfer.services.TransferService;
 
 @RestController
@@ -20,5 +24,11 @@ public class TransferResource {
     @GetMapping(value = "/{uuid}")
     public ResponseEntity<TransferDTO> findByUuid(@PathVariable(value = "uuid") String uuid) {
         return ResponseEntity.ok().body(service.findByUuid(uuid));
+    }
+
+    @PostMapping
+    public ResponseEntity<TransferDTO> createTransfer(@RequestBody TransferRequestDTO transfer) {
+        HttpStatus status = HttpStatus.CREATED;
+        return ResponseEntity.status(status.value()).body(service.createTransfer(transfer));
     }
 }
