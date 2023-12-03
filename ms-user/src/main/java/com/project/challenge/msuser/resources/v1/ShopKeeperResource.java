2c412e6b849fb403fab330686d0cfdd89a56430c
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-03 04:22:02 -0300

    Create ShopKeeperResource.java

---
+++ ms-user/src/main/java/com/project/challenge/msuser/resources/v1/ShopKeeperResource.java
@@ -0,0 +1,24 @@
+package com.project.challenge.msuser.resources.v1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.challenge.msuser.DTOs.user.v1.ShopKeeperDTO;
+import com.project.challenge.msuser.services.v1.ShopKeeperService;
+
+@RestController
+@RequestMapping(value = "/ppchallenge/v1/shopkeepers")
+public class ShopKeeperResource {
+
+    @Autowired
+    private ShopKeeperService service;
+
+    @GetMapping(value = "/{uuid}")
+    public ResponseEntity<ShopKeeperDTO> findByUuid(@PathVariable(name = "uuid") String uuid) {
+        return ResponseEntity.ok().body(service.findByUuid(uuid));
+    }
+}
