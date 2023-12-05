a3e9a1253d242eb2f2b3bbf2be571d920b8ba7bc
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-05 11:15:37 -0300

    creation of methods for balance changes

--- ms-user/src/main/java/com/project/challenge/msuser/resources/v1/UserResource.java
+++ ms-user/src/main/java/com/project/challenge/msuser/resources/v1/UserResource.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.challenge.msuser.DTOs.user.v1.UserBalanceDTO;
 import com.project.challenge.msuser.DTOs.user.v1.UserDTO;
 import com.project.challenge.msuser.services.v1.UserService;
 
@@ -20,6 +23,21 @@ public class UserResource {
     @GetMapping(value = "/search/{uuid}")
     public ResponseEntity<UserDTO> getUserByUuid(@PathVariable(name = "uuid") String uuid) {
         return ResponseEntity.ok().body(service.getUserByUuid(uuid));
+    }
+
+    @GetMapping(value = "/search/common-user/{uuid}")
+    public ResponseEntity<UserDTO> getUserByCommonUserUuid(@PathVariable(name = "uuid") String uuid) {
+        return ResponseEntity.ok().body(service.getUserByCommonUserUuid(uuid));
+    }
+
+    @GetMapping(value = "/search/shopkeeper/{uuid}")
+    public ResponseEntity<UserDTO> getUserByShopKeeperUuid(@PathVariable(name = "uuid") String uuid) {
+        return ResponseEntity.ok().body(service.getUserByShopKeeperUuid(uuid));
+    }
+
+    @PostMapping(value = "/balances")
+    public ResponseEntity<UserDTO> updateBalance(@RequestBody UserBalanceDTO userBalance) {
+        return ResponseEntity.ok().body(service.saveBalanceToUser(userBalance));
     }
 
 }
--- ms-user/src/main/java/com/project/challenge/msuser/services/v1/UserService.java
+++ ms-user/src/main/java/com/project/challenge/msuser/services/v1/UserService.java
@@ -3,6 +3,7 @@ package com.project.challenge.msuser.services.v1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.project.challenge.msuser.DTOs.user.v1.UserBalanceDTO;
 import com.project.challenge.msuser.DTOs.user.v1.UserDTO;
 import com.project.challenge.msuser.entities.User;
 import com.project.challenge.msuser.infra.exceptions.UserNotFound;
@@ -21,9 +22,44 @@ public class UserService {
                 .toDTO(findUserByUuid(uuid));
     }
 
+    public UserDTO getUserByCommonUserUuid(String uuid) {
+        return MapperUser
+                .getInstance()
+                .toDTO(findUserByCommonUserUuid(uuid));
+    }
+
+    public UserDTO getUserByShopKeeperUuid(String uuid) {
+        return MapperUser
+                .getInstance()
+                .toDTO(findUserByShopKeeperUuid(uuid));
+    }
+
+    public UserDTO saveBalanceToUser(UserBalanceDTO userBalanceDTO) {
+        User user = findUserByUuid(userBalanceDTO.getUuid());
+        user.setBalance(userBalanceDTO.getBalance());
+        saveUser(user);
+        return MapperUser
+                .getInstance()
+                .toDTO(user);
+    }
+
+    private void saveUser(User user) {
+        repo.save(user);
+    }
+
     private User findUserByUuid(String uuid) {
         return repo.findByUuid(uuid)
                 .orElseThrow(() -> new UserNotFound(String.format("User with UUID (%s) not found.", uuid)));
+    }
+
+    private User findUserByCommonUserUuid(String uuid) {
+        return repo.findByCommonUserUuid(uuid)
+                .orElseThrow(() -> new UserNotFound(String.format("User with Common User UUID (%s) not found.", uuid)));
+    }
+
+    private User findUserByShopKeeperUuid(String uuid) {
+        return repo.findByShopKeeperUuid(uuid)
+                .orElseThrow(() -> new UserNotFound(String.format("User with Shopkeeper UUID (%s) not found.", uuid)));
     }
 
 }
