87e68577feb950e32aa517994c49bd6665a4aa55
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-05 11:14:55 -0300

    Create UserBalanceDTO.java

---
+++ ms-user/src/main/java/com/project/challenge/msuser/DTOs/user/v1/UserBalanceDTO.java
@@ -0,0 +1,40 @@
+package com.project.challenge.msuser.DTOs.user.v1;
+
+import java.io.Serializable;
+
+public class UserBalanceDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String uuid;
+    private Double balance;
+
+    public UserBalanceDTO() {
+    }
+
+    public UserBalanceDTO(String uuid, Double balance) {
+        this.uuid = uuid;
+        this.balance = balance;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "UserBalanceDTO [uuid=" + uuid + ", balance=" + balance + "]";
+    }
+
+}
