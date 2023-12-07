23dd864dcd17c4e9f862c40903c5148c6a5b3ee3
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-07 12:08:50 -0300

    add validators

--- ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/services/TransferService.java
+++ ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/services/TransferService.java
@@ -1,11 +1,15 @@
 package com.project.challenge.mstransfer.mstransfer.services;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import com.project.challenge.mstransfer.mstransfer.DTOs.transfer.v1.TransferDTO;
 import com.project.challenge.mstransfer.mstransfer.DTOs.transfer.v1.TransferRequestDTO;
 import com.project.challenge.mstransfer.mstransfer.DTOs.user.v1.ReceiverBaseDTO;
+import com.project.challenge.mstransfer.mstransfer.clients.MockClient;
 import com.project.challenge.mstransfer.mstransfer.clients.UserClient;
 import com.project.challenge.mstransfer.mstransfer.entities.Transfer;
 import com.project.challenge.mstransfer.mstransfer.enumerations.TransferStatus;
@@ -14,7 +18,8 @@ import com.project.challenge.mstransfer.mstransfer.infra.exceptions.TransferNotF
 import com.project.challenge.mstransfer.mstransfer.mappers.v1.MapperTransfer;
 import com.project.challenge.mstransfer.mstransfer.repositories.TransferRepository;
 import com.project.challenge.mstransfer.mstransfer.validators.DifferentReceiverAndSender;
-import com.project.challenge.mstransfer.mstransfer.validators.ReceiverValidator;
+import com.project.challenge.mstransfer.mstransfer.validators.InsufficientFunds;
+import com.project.challenge.mstransfer.mstransfer.validators.MockAuthValidation;
 import com.project.challenge.mstransfer.mstransfer.validators.TransferValueValidator;
 import com.project.challenge.mstransfer.mstransfer.validators.ValidatorManager;
 
@@ -27,6 +32,9 @@ public class TransferService {
     @Autowired
     private UserClient userClient;
 
+    @Autowired
+    private MockClient mockClient;
+
     public TransferDTO findByUuid(String uuid) {
         return MapperTransfer
                 .getInstance()
@@ -36,22 +44,41 @@ public class TransferService {
     public TransferDTO createTransfer(TransferRequestDTO transferRequestDTO) {
         transferRequestValidation(transferRequestDTO);
         Transfer transfer = generateTransferByTransferRequest(transferRequestDTO);
-        transactionValidation(transfer);
+        Map<String, ?> differentReceiverAndSenderResult = transactionValidationWithReturnedValue(transfer)
+                .get("DifferentReceiverAndSender");
+        if (!ObjectUtils.isEmpty(differentReceiverAndSenderResult)) {
+            transactionValidationWithoutReturnedValue(differentReceiverAndSenderResult, transfer);
+            executeTransaction(differentReceiverAndSenderResult, transfer);
+        }
         saveTransfer(transfer);
         return MapperTransfer
                 .getInstance()
                 .toDTO(transfer);
     }
 
-    private void transactionValidation(Transfer transfer) {
+    private void executeTransaction(Map<String, ?> senderReceiver, Transfer transfer) {
+
+    }
+
+    private void transactionValidationWithoutReturnedValue(Map<String, ?> senderReceiver, Transfer transfer) {
         ValidatorManager<Transfer> transferManager = new ValidatorManager<>(
-                new DifferentReceiverAndSender(userClient));
+                new InsufficientFunds(senderReceiver));
         transferManager.executeAll(transfer);
+
+        ValidatorManager<String> mockTransferManager = new ValidatorManager<>(
+                new MockAuthValidation(mockClient));
+        mockTransferManager.executeAll("Autorizado");
+    }
+
+    private Map<String, Map<String, ?>> transactionValidationWithReturnedValue(Transfer transfer) {
+        ValidatorManager<Transfer> transferManager = new ValidatorManager<>();
+        transferManager.addMapValidator(new DifferentReceiverAndSender(userClient));
+        transferManager.executeMapValidation(transfer);
+        return transferManager.getMapResults();
     }
 
     private void transferRequestValidation(TransferRequestDTO transfer) {
         ValidatorManager<TransferRequestDTO> validatorManager = new ValidatorManager<>(
-                new ReceiverValidator(userClient),
                 new TransferValueValidator());
         validatorManager.executeAll(transfer);
     }
