c529c5f27dc72d37b252417cfc7eaaa9582445df
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-07 12:48:05 -0300

    update to receive Transfer in execute method

--- ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/validators/MockAuthValidation.java
+++ ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/validators/MockAuthValidation.java
@@ -1,10 +1,11 @@
 package com.project.challenge.mstransfer.mstransfer.validators;
 
 import com.project.challenge.mstransfer.mstransfer.clients.MockClient;
+import com.project.challenge.mstransfer.mstransfer.entities.Transfer;
 import com.project.challenge.mstransfer.mstransfer.infra.exceptions.ExternalServiceException;
 import com.project.challenge.mstransfer.mstransfer.interfaces.IValidator;
 
-public class MockAuthValidation implements IValidator<String> {
+public class MockAuthValidation implements IValidator<Transfer> {
 
     private MockClient mockClient;
 
@@ -16,8 +17,8 @@ public class MockAuthValidation implements IValidator<String> {
     }
 
     @Override
-    public void execute(String obj) {
-        if (!mockClient.getMockAuth().getBody().message().equals(obj))
+    public void execute(Transfer transfer) {
+        if (!mockClient.getMockAuth().getBody().message().equals("Autorizado"))
             throw new ExternalServiceException("Unauthorized transaction.");
 
     }
