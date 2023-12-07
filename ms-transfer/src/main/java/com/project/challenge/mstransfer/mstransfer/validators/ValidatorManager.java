622933c33d569107d8acbc47204551dda17792a1
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-07 10:56:27 -0300

    add suport to get returned results in validations

--- ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/validators/ValidatorManager.java
+++ ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/validators/ValidatorManager.java
@@ -1,10 +1,19 @@
 package com.project.challenge.mstransfer.mstransfer.validators;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.util.ObjectUtils;
+
 import com.project.challenge.mstransfer.mstransfer.interfaces.IValidator;
+import com.project.challenge.mstransfer.mstransfer.interfaces.IValidatorWithReturn;
 
 public class ValidatorManager<T> {
 
-    public IValidator<T>[] validators;
+    private IValidator<T>[] validators;
+    private Map<String, IValidatorWithReturn<T>> mapValidators = new HashMap<>();
+
+    private Map<String, Map<String, ?>> mapResults = new HashMap<>();
 
     public ValidatorManager() {
     }
@@ -18,9 +27,27 @@ public class ValidatorManager<T> {
         return validators;
     }
 
+    public Map<String, Map<String, ?>> getMapResults() {
+        return mapResults;
+    }
+
     public void executeAll(T object) {
         for (IValidator<T> validator : validators) {
             validator.execute(object);
         }
+    }
+
+    public ValidatorManager<T> addMapValidator(IValidatorWithReturn<T> validator) {
+        mapValidators.put(validator.getClass().getSimpleName(), validator);
+        return this;
+    }
+
+    public ValidatorManager<T> executeMapValidation(T object) {
+        for (Map.Entry<String, IValidatorWithReturn<T>> entry : mapValidators.entrySet()) {
+            Map<String, ?> result = entry.getValue().executeValidationWithReturn(object);
+            if (!ObjectUtils.isEmpty(result))
+                mapResults.put(entry.getKey(), entry.getValue().executeValidationWithReturn(object));
+        }
+        return this;
     }
 }
