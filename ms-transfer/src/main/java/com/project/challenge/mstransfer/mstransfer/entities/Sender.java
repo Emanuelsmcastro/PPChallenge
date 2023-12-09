6fd0894d4059ca358241efd3e0250307ec1c9b76
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-09 11:47:36 -0300

    Create Sender.java

---
+++ ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/entities/Sender.java
@@ -0,0 +1,46 @@
+package com.project.challenge.mstransfer.mstransfer.entities;
+
+import com.project.challenge.mstransfer.mstransfer.DTOs.user.v1.CommonUserDTO;
+import com.project.challenge.mstransfer.mstransfer.DTOs.user.v1.ShopKeeperDTO;
+import com.project.challenge.mstransfer.mstransfer.DTOs.user.v1.UserBalanceDTO;
+import com.project.challenge.mstransfer.mstransfer.DTOs.user.v1.UserDTO;
+import com.project.challenge.mstransfer.mstransfer.interfaces.IReceiver;
+import com.project.challenge.mstransfer.mstransfer.interfaces.ISender;
+
+public class Sender extends UserDTO implements ISender {
+
+    public Sender() {
+
+    }
+
+    public Sender(String uuid, String fullName, String email, Double balance, ShopKeeperDTO shopKeeper,
+            CommonUserDTO commonUser) {
+        super(uuid, fullName, email, balance, shopKeeper, commonUser);
+    }
+
+    @Override
+    public String toString() {
+        return "Sender [uuid=" + super.getUuid() + ", email=" + super.getEmail() + ", shopKeeper="
+                + super.getShopKeeper() + ", commonUser="
+                + super.getCommonUser() + "]";
+    }
+
+    @Override
+    public void toSend(IReceiver receiver, Double value) {
+        super.setBalance(super.getBalance() - value);
+        receiver.toReceive(value);
+    }
+
+    @Override
+    public Double getTransferLimit() {
+        return super.getCommonUser().getTransferLimit();
+    }
+
+    @Override
+    public UserBalanceDTO getUserBalanceDTO() {
+        return new UserBalanceDTO(
+                super.getUuid(),
+                super.getBalance());
+    }
+
+}
