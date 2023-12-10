598769bf6787eb08b0bcf0afa88576fc3ea07f29
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-10 06:38:20 -0300

    update to use paged list

--- ms-user/src/main/java/com/project/challenge/msuser/services/v1/UserService.java
+++ ms-user/src/main/java/com/project/challenge/msuser/services/v1/UserService.java
@@ -1,6 +1,14 @@
 package com.project.challenge.msuser.services.v1;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
 
 import com.project.challenge.msuser.DTOs.user.v1.UserBalanceDTO;
@@ -9,12 +17,26 @@ import com.project.challenge.msuser.entities.User;
 import com.project.challenge.msuser.infra.exceptions.UserNotFound;
 import com.project.challenge.msuser.mappers.user.v1.MapperUser;
 import com.project.challenge.msuser.repositories.UserRepository;
+import com.project.challenge.msuser.resources.v1.UserResource;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository repo;
+
+    @Autowired
+    PagedResourcesAssembler<UserDTO> assembler;
+
+    public PagedModel<EntityModel<UserDTO>> findAllUsers(Pageable pageable) {
+        Page<User> userPage = repo.findAll(pageable);
+        Page<UserDTO> userPageDTO = MapperUser
+                .getInstance()
+                .toDTOList(userPage);
+        Link link = linkTo(methodOn(UserResource.class).findAllUsers(pageable.getPageNumber(), pageable.getPageSize()))
+                .withSelfRel();
+        return assembler.toModel(userPageDTO, link);
+    }
 
     public UserDTO getUserByUuid(String uuid) {
         return MapperUser
