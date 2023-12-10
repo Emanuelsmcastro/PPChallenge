6c6c268de9d0c5b116d8757bdb8a0d83d32c5cd0
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-09 21:50:39 -0300

    Delete ISending.java

--- ms-user/src/main/java/com/project/challenge/msuser/interfaces/ISending.java
+++
@@ -1,8 +0,0 @@
-package com.project.challenge.msuser.interfaces;
-
-import com.project.challenge.msuser.entities.User;
-
-public interface ISending {
-
-    void sending(Double value, User user);
-}
