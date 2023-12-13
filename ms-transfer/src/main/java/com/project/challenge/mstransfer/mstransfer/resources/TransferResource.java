576593ee477b53e31e828b7e7efeea1fb25b739f
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-13 11:16:53 -0300

    finish swagger doc in ms-transfer

--- ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/resources/TransferResource.java
+++ ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/resources/TransferResource.java
@@ -12,9 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.project.challenge.mstransfer.mstransfer.DTOs.transfer.v1.TransferDTO;
 import com.project.challenge.mstransfer.mstransfer.DTOs.transfer.v1.TransferRequestDTO;
+import com.project.challenge.mstransfer.mstransfer.infra.exceptions.DTOs.BasicResponseExceptionDTO;
 import com.project.challenge.mstransfer.mstransfer.services.TransferService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@Tag(name = "Transfer Resource V1", description = "These endpoints are responsible for handling all available functionalities related to the transfer.")
 @RequestMapping(value = "/ppchallenge/v1/transfers")
 public class TransferResource {
 
@@ -22,11 +30,27 @@ public class TransferResource {
     private TransferService service;
 
     @GetMapping(value = "/{uuid}")
+    @Operation(summary = "Find transfer.", description = "Return transfer by uuid.", responses = {
+    		@ApiResponse(description = "Success", responseCode = "200", content = {
+    				@Content(mediaType = "application/json", schema = @Schema(implementation = TransferDTO.class))
+    		}),
+    		@ApiResponse(description = "Not found", responseCode = "404", content = {
+    				@Content(mediaType = "application/json", schema = @Schema(implementation = BasicResponseExceptionDTO.class))
+    		})
+    })
     public ResponseEntity<TransferDTO> findByUuid(@PathVariable(value = "uuid") String uuid) {
         return ResponseEntity.ok().body(service.findByUuid(uuid));
     }
 
     @PostMapping
+    @Operation(summary = "Transfer request.", description = "Returns a transfer after executing it.", responses = {
+    		@ApiResponse(description = "Success", responseCode = "200", content = {
+    				@Content(mediaType = "application/json", schema = @Schema(implementation = TransferDTO.class))
+    		}),
+    		@ApiResponse(description = "Fail", responseCode = "400", content = {
+    				@Content(mediaType = "application/json", schema = @Schema(implementation = BasicResponseExceptionDTO.class))
+    		})
+    })
     public ResponseEntity<TransferDTO> createTransfer(@RequestBody TransferRequestDTO transfer) {
         HttpStatus status = HttpStatus.CREATED;
         return ResponseEntity.status(status.value()).body(service.createTransfer(transfer));
