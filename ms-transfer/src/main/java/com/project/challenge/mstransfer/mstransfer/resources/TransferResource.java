95581f71678971466711f6afad472cd762932a0f
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-16 21:45:30 -0300

    config resource server

--- ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/resources/TransferResource.java
+++ ms-transfer/src/main/java/com/project/challenge/mstransfer/mstransfer/resources/TransferResource.java
@@ -3,6 +3,7 @@ package com.project.challenge.mstransfer.mstransfer.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,6 +43,7 @@ public class TransferResource {
         return ResponseEntity.ok().body(service.findByUuid(uuid));
     }
 
+    @CrossOrigin(origins = "http://localhost:8765")
     @PostMapping
     @Operation(summary = "Transfer request.", description = "Returns a transfer after executing it.", responses = {
     		@ApiResponse(description = "Success", responseCode = "200", content = {
