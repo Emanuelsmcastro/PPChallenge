6936f976fbf73d754940109dc97c79a87a052ccc
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-12-23 14:17:45 -0300

    add cors config

--- ms-user/src/main/java/com/project/challenge/msuser/resources/v1/ShopKeeperResource.java
+++ ms-user/src/main/java/com/project/challenge/msuser/resources/v1/ShopKeeperResource.java
@@ -2,6 +2,7 @@ package com.project.challenge.msuser.resources.v1;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class ShopKeeperResource {
     private ShopKeeperService service;
 
     @GetMapping(value = "/{uuid}")
+    @CrossOrigin(origins = {"http://localhost:8765"})
     public ResponseEntity<ShopKeeperDTO> findByUuid(@PathVariable(value = "uuid") String uuid) {
         return ResponseEntity.ok().body(service.findByUuid(uuid));
     }
